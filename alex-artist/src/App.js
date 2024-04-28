843a9c25f8c1ccb023e3b2e61c4fa7eff71a7bff
dIvashinin <dmitry.ivashinin@gmail.com>
2024-04-28 21:55:58 +0200

    first changes inside index page

--- alex-artist/src/App.js
+++ alex-artist/src/App.js
@@ -5,18 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          #alexanderskorobogatov
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
