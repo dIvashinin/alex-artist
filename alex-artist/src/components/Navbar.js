f01cb12e44c1e994f8dd97eb5ed591215426a3c1
dIvashinin <dmitry.ivashinin@gmail.com>
2024-04-29 21:29:04 +0200

    offcanvas working, styled with css

--- alex-artist/src/components/Navbar.js
+++ alex-artist/src/components/Navbar.js
@@ -11,17 +11,25 @@ function Navbar() {
     const handleShow = () => setShow(true);
 
   return (
-    <>
-     <Button variant="danger" onClick={handleShow} className="me-2"></Button>
-     <Offcanvas show={show} onHide={handleClose} >
+    <div className='off-canvas-container'>
+     <Button variant="danger" onClick={handleShow} className="me-2" >MENU</Button>
+     <Offcanvas className="off-canvas-main" show={show} onHide={handleClose} placement="top" >
      <Offcanvas.Header closeButton>
      <Offcanvas.Title>MENU</Offcanvas.Title>
    </Offcanvas.Header>
-   <Offcanvas.Body>
-     Here we gonna have stuff
+     
+   <Offcanvas.Body className='off-canvas-body'>
+    <p>paintings</p>
+    <p>sculptures</p>
+    <p>installations</p>
+    <p>drawings</p>
+    <p>art</p>
+    <p>exhibitions</p>
+    <p>bio</p>
+    <p>contact</p>
    </Offcanvas.Body>
  </Offcanvas>
-    </>
+    </div>
   )
 }
 export default Navbar
