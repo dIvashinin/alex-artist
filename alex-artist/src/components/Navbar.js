0a4292db3c3f32d93d608fbdbd0b19ad9746b42b
dIvashinin <dmitry.ivashinin@gmail.com>
2024-04-29 20:54:59 +0200

    installed react bootstrap

--- alex-artist/src/components/Navbar.js
+++ alex-artist/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Button, Offcanvas } from 'react-bootstrap';
 
 
 function Navbar() {
   return (
     <>
-      <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Enable both scrolling & backdrop</button>
+      <Button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Enable both scrolling & backdrop</Button>
 
 <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
   <div class="offcanvas-header">
     <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    <Button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></Button>
   </div>
   <div class="offcanvas-body">
     <p>Try scrolling the rest of the page to see this option in action.</p>
