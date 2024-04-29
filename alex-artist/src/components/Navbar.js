9815b9304815e73ce87edeecd4b7b58a307af1de
dIvashinin <dmitry.ivashinin@gmail.com>
2024-04-29 21:08:46 +0200

    offcanvas working

--- alex-artist/src/components/Navbar.js
+++ alex-artist/src/components/Navbar.js
@@ -1,23 +1,27 @@
 import React from 'react'
-import { Button, Offcanvas } from 'react-bootstrap';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 
 function Navbar() {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
   return (
     <>
-      <Button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Enable both scrolling & backdrop</Button>
-
-<div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
-    <Button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></Button>
-  </div>
-  <div class="offcanvas-body">
-    <p>Try scrolling the rest of the page to see this option in action.</p>
-  </div>
-</div>
+     <Button variant="danger" onClick={handleShow} className="me-2"></Button>
+     <Offcanvas show={show} onHide={handleClose} >
+     <Offcanvas.Header closeButton>
+     <Offcanvas.Title>MENU</Offcanvas.Title>
+   </Offcanvas.Header>
+   <Offcanvas.Body>
+     Here we gonna have stuff
+   </Offcanvas.Body>
+ </Offcanvas>
     </>
   )
 }
-
 export default Navbar
