f827b06aee0a0b081728e93f42241bdb6ee59d8d
dIvashinin <dmitry.ivashinin@gmail.com>
2024-05-05 23:41:21 +0200

    added insta link to contact page

--- alex-artist/src/components/ContactPage.js
+++ alex-artist/src/components/ContactPage.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+
 
 function ContactPage() {
+  const url = "https://www.instagram.com/alexanderskorobogatov/";
   return (
     <div>
       here go contacts
+      <p><a href="https://www.instagram.com/alexanderskorobogatov/" className='insta-link' target="_blank" rel="noopener noreferrer">instagram</a></p>
+
     </div>
   )
 }
