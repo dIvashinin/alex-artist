4a51ca33d6d4e4396650724b7a882f25b9b84d94
dIvashinin <dmitry.ivashinin@gmail.com>
2024-05-19 21:45:00 +0200

    adding bootstrap modal component as an option

---
+++ alex-artist/src/components/ImageModal.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+const ImageModal = ({ show, handleClose, imageUrl }) => {
+  return (<Modal show={show} onHide={handleClose}>
+    <Modal.Header closeButton>
+      <Modal.Title>Image</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <img src={imageUrl} alt="modal pic" className="img-fluid" />
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={handleClose}>
+        Close
+      </Button>
+    </Modal.Footer>
+  </Modal>
+);
+};
+
+export default ImageModal;
