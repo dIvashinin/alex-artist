011949af80033841522e2753bd6f82303b34b00f
dIvashinin <dmitry.ivashinin@gmail.com>
2024-05-19 21:47:48 +0200

    adding a separate masonry item component

---
+++ alex-artist/src/components/MasonryItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MasonryItem = ({ url, onClick }) => {
+  return (
+    <div className="masonry-item" onClick={() => onClick(url)}>
+      <img src={url} className="pic" alt="pic" style={{ width: '100%', cursor: 'pointer' }} />
+    </div>
+  );
+};
+
+export default MasonryItem;
