a056273ab1c68479934955520971ec4bdae01d95
dIvashinin <dmitry.ivashinin@gmail.com>
2024-05-23 23:56:13 +0200

    added modal in art page on clicking every single image, styling

--- alex-artist/src/components/MasonryItem.js
+++ alex-artist/src/components/MasonryItem.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const MasonryItem = ({ url, onClick }) => {
   return (
     <div className="masonry-item" onClick={() => onClick(url)}>
-      <img src={url} className="pic" alt="pic" 
-      // style={{ width: '100%', cursor: 'pointer' }} 
+      <img
+        src={url}
+        className="pic"
+        alt="pic"
+        // style={{ width: '100%', cursor: 'pointer' }}
       />
     </div>
   );
