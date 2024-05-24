c802dbd570a3053cd383688f5edf45ebfe3f3dba
dIvashinin <dmitry.ivashinin@gmail.com>
2024-05-24 20:15:21 +0200

    styling modal

--- alex-artist/src/components/ImageModal.js
+++ alex-artist/src/components/ImageModal.js
@@ -3,9 +3,9 @@ import { Modal, Button } from 'react-bootstrap';
 
 const ImageModal = ({ show, handleClose, imageUrl }) => {
   return (
-  <Modal show={show} onHide={handleClose}>
+  <Modal className='modal-main' show={show} onHide={handleClose}>
     <Modal.Header closeButton>
-      <Modal.Title>Image</Modal.Title>
+      <Modal.Title className='modal-title'>Image</Modal.Title>
     </Modal.Header>
     <Modal.Body>
       <img src={imageUrl} alt="modal pic" className="img-fluid" />
