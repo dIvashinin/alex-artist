9fae31c1447dcab6cfe4195327766ce4f0f4d228
dIvashinin <dmitry.ivashinin@gmail.com>
2024-05-28 18:59:53 +0200

    adding lorem ipsum to exhibitions, styling

--- alex-artist/src/components/ExhibitionsPage.js
+++ alex-artist/src/components/ExhibitionsPage.js
@@ -2,7 +2,63 @@ import React from 'react'
 
 function ExhibitionsPage() {
   return (
-    <div>ExhibitionsPage</div>
+    <div>here go exhibitions
+
+<h3>2024</h3>
+<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+        odio. Praesent libero. Sed cursus ante dapibus diam. 
+        </p>
+        <p>Sed nisi. Nulla
+        quis sem at nibh elementum imperdiet. Duis sagittis ipsum. {" "}
+      </p>
+
+      <p>
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+        inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim
+        lacinia nunc. Curabitur tortor. 
+        </p>
+        <p>Pellentesque nibh. Aenean quam. In
+        scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem.
+        Proin ut ligula vel nunc egestas porttitor. </p>
+        <p>Morbi lectus risus, iaculis
+        vel, suscipit quis, luctus non, massa.
+      </p>
+
+      <p>
+        {" "}
+        Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus
+        metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque
+        volutpat condimentum velit.
+      </p>
+      <h3>2023</h3>
+      <p>
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+        inceptos himenaeos. Nam nec ante. </p>
+        <p>Sed lacinia, urna non tincidunt
+        mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis.
+        Nulla facilisi. Ut fringilla. Suspendisse potenti. </p>
+        <p>Nunc feugiat mi a
+        tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam
+        ultrices.
+      </p>
+      <p>
+        {" "}
+        Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer
+        euismod lacus luctus magna. </p>
+        <p>Quisque cursus, metus vitae pharetra auctor,
+        sem massa mattis sem, at interdum magna augue eget diam.
+      </p>
+      <h3>2022</h3>
+      <p>
+        {" "}
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia Curae; Morbi lacinia molestie dui. </p>
+        <p>Praesent blandit dolor. Sed
+        non quam. In vel mi sit amet augue congue elementum.
+      </p>
+
+    </div>
   )
 }
 
