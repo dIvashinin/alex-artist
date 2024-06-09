b8c50ce1dce00fd864e2be825de6c3c03b22c841
dIvashinin <dmitry.ivashinin@gmail.com>
2024-06-09 23:28:38 +0200

    customising bootstrap modal element, left narrow border, very minimalistic

--- alex-artist/src/components/ImageModal.js
+++ alex-artist/src/components/ImageModal.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
 
 const ImageModal = ({ show, handleClose, imageUrl }) => {
   return (
-  <Modal className='modal-main' show={show} onHide={handleClose}>
-    <Modal.Header 
-    // closeButton
-    >
-      <Modal.Title className='modal-title'></Modal.Title>
-    </Modal.Header>
-    <Modal.Body className='modal-body'>
-      <img src={imageUrl} alt="modal pic" className="img-fluid" />
-    </Modal.Body>
-    {/* <Modal.Footer>
+    <Modal className="modal-main" show={show} onHide={handleClose}>
+      <Modal.Header
+      // closeButton
+      >
+        <Modal.Title className="modal-title"></Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="modal-body">
+        <img src={imageUrl} alt="modal pic" className="img-fluid" />
+      </Modal.Body>
+      {/* <Modal.Footer>
       <Button variant="secondary" onClick={handleClose}>
         Close
       </Button>
     </Modal.Footer> */}
-  </Modal>
-);
+    </Modal>
+  );
 };
 
 export default ImageModal;
